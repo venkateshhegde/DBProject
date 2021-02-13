bdabb9b26c2d69d19191abd5c45ab13c22a0e042
Venkatesh Hegde <venkateshhegde@yahoo.com>
2021-02-13 11:21:44 -0500

    SQL Files add to DataGrip

--- DBStock.sql
+++ DBStock.sql
@@ -6,3 +6,22 @@ Select STOCKSYMBOL, COUNT(STOCKSYMBOL) as count
 from VEN."StockReco"
 group by STOCKSYMBOL
 order by count desc
+
+
+
+-- auto-generated definition
+-- auto-generated definition
+
+drop table Ven."PositionTable";
+create table Ven."PositionTable"
+(
+    "Symbol"       VARCHAR(100) not null,
+    "Quantity"     DOUBLE not null,
+    "CostPerShare" DOUBLE not null,
+    "AssetType"    VARCHAR(100) not null
+);
+
+create index POSITIONTABLE_SYMBOL_INDEX
+	on "PositionTable" ("Symbol");
+
+
